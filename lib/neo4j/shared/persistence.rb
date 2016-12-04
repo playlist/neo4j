a13f95db10fc500d83d609a5dc3011a174924117
Steve Petersen <steve@maitapetersen.com>
2016-12-03 20:27:16 -0800

    reapply time changes to this branch

--- lib/neo4j/shared/persistence.rb
+++ lib/neo4j/shared/persistence.rb
@@ -257,11 +257,11 @@ module Neo4j::Shared
     end
 
     def update_magic_properties
-      self.updated_at = DateTime.now if respond_to?(:updated_at=) && (updated_at.nil? || (changed? && !updated_at_changed?))
+      self.updated_at = Time.now if respond_to?(:updated_at=) && (updated_at.nil? || (changed? && !updated_at_changed?))
     end
 
     def inject_timestamps!
-      now = DateTime.now
+      now = Time.now
       self.created_at ||= now if respond_to?(:created_at=)
       self.updated_at ||= now if respond_to?(:updated_at=)
     end
